b0697982ff08660caedd11fe17e3342eb7654185
aiyarankith <aiyar.ankith@gmail.com>
2015-04-20 00:45:20 -0700

    Base Setup - 4/20

---
+++ mysite/mysite/urls.py
@@ -0,0 +1,22 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+from polls.views import *
+
+urlpatterns = [
+    # Examples:
+    # url(r'^$', 'mysite.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^index/', 'polls.views.index', name='index'),
+    url(r'^blog/', 'polls.views.blog', name='blog'),
+    url(r'^pricing/', 'polls.views.pricing', name='pricing'),
+    url(r'^contact-us/', 'polls.views.contactus', name='contact-us'),
+    url(r'^about-us/', 'polls.views.aboutus', name='about-us'),
+    url(r'^blog-item/', 'polls.views.blogitem', name='blog-item'),
+    url(r'^portfolio/', 'polls.views.portfolio', name='portfolio'),
+    url(r'^services/', 'polls.views.services', name='services'),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
---
+++ mysite/polls/views.py
@@ -0,0 +1,56 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+from django.template import RequestContext, loader
+
+def index(request):
+    template = loader.get_template('index.html')
+    context = RequestContext(request, {
+        
+    })
+    return HttpResponse(template.render(context))
+
+def blog(request):
+    template = loader.get_template('blog.html')
+    context = RequestContext(request, {
+        
+    })
+    return HttpResponse(template.render(context))
+
+def pricing(request):
+    template = loader.get_template('pricing.html')
+    context = RequestContext(request, {
+        
+    })
+    return HttpResponse(template.render(context))
+def aboutus(request):
+    template = loader.get_template('about-us.html')
+    context = RequestContext(request, {
+        
+    })
+    return HttpResponse(template.render(context))
+def blogitem(request):
+    template = loader.get_template('blog-item.html')
+    context = RequestContext(request, {
+        
+    })
+    return HttpResponse(template.render(context))
+def contactus(request):
+    template = loader.get_template('contact-us.html')
+    context = RequestContext(request, {
+        
+    })
+    return HttpResponse(template.render(context))
+def portfolio(request):
+    template = loader.get_template('portfolio.html')
+    context = RequestContext(request, {
+        
+    })
+    return HttpResponse(template.render(context))
+def services(request):
+    template = loader.get_template('services.html')
+    context = RequestContext(request, {
+        
+    })
+    return HttpResponse(template.render(context))
